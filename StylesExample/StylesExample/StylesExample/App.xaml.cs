06b6fbd307429a191c6ea8d15d48e95b3fd1f73c
Admin <Admin>
2016-09-06 14:48:11 +0200

    Added aspectfit to backgroundimage

--- StylesExample/StylesExample/StylesExample/App.xaml.cs
+++ StylesExample/StylesExample/StylesExample/App.xaml.cs
@@ -13,7 +13,7 @@ namespace StylesExample
         {
             InitializeComponent();
 
-            MainPage = new StylesExample.MainPage();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
