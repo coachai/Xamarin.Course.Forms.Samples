c1da1f4eb2abb7a214952a035918df764f2f3082
bvi@hivepeople.dk <snb@hivepeople.dk>
2016-12-02 11:41:36 +0100

    Added navigationpage exampe

---
+++ NavigationPageExample/NavigationPageExample/NavigationPageExample/Page1.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace NavigationPageExample
+{
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private async void PushPage2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2());
+        }
+    }
+}
---
+++ NavigationPageExample/NavigationPageExample/NavigationPageExample/Page2.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace NavigationPageExample
+{
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private async void PushPage2(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
